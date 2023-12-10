07fde7787c5a596dd313f1c77412470b08a0f9f8
Mateus Souza <msouza954@gmail.com>
2023-12-10 02:50:21 -0300

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,204 @@
-import Image from 'next/image'
+"use client";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useCallback } from "react";
+import axios from "axios";
 
-export default function Home() {
+//Schema
+const createUserFormSchema = z.object({
+  name: z.string().min(1, "Nome obrigatório"),
+  cpf: z.string().max(11),
+  birth: z.date().max(new Date(), {
+    message: "Você precisa ter mais de 18 anos para continuar",
+  }),
+  contact: z.string().nonempty("Contato obrigatório"),
+  address: z.object({
+    zipCode: z.string().min(8, "CEP obrigatório"),
+    state: z.string().max(2, "Selecione o Estado"),
+    city: z.string().min(1, "Selecione a Cidade"),
+    street: z.string().min(1, "Informe a Rua"),
+    district: z.string().min(1, "Informe o Bairro"),
+    number: z.string().min(9, "Informe o Número da Casa").max(40),
+    complement: z.string(),
+  }),
+  email: z.string().min(1, "Email obrigatório").email("Email inválido"),
+  password: z.string().min(10, "A senha precisa ter no mínimo 10 caracteres"),
+});
+
+type userFormData = z.infer<typeof createUserFormSchema>;
+
+export default function Form() {
+  const {
+    watch,
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<userFormData>({
+    criteriaMode: "all",
+    mode: "all",
+    resolver: zodResolver(createUserFormSchema),
+    defaultValues: {
+      name: "",
+      cpf: "",
+      birth: new Date(),
+      contact: "",
+      address: {
+        zipCode: "",
+        state: "",
+        city: "",
+        street: "",
+        district: "",
+        number: "",
+        complement: "",
+      },
+      email: "",
+      password: "",
+    },
+  });
+
+  const createUser = (data: any) => {
+    console.log(data);
+  };
+
+  const handleFetchAdress = useCallback(async (zipCode: string) => {
+    const { data } = await axios.get(
+      `http://viacep.com.br/ws/${zipCode}/json/`
+    );
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex h-full flex-col items-center text-custom-white-100 justify-center bg-white">
+      <form
+        onSubmit={handleSubmit(createUser)}
+        className="flex flex-col h-full mt-24 gap-8 w-full max-w-[564px]"
+      >
+        <div className="input-area">
+          <label className="text-xs font-semibold">Nome Completo</label>
+          <input
+            type="text"
+            placeholder="Cauê Ian Benedito Araújo"
+            className="input-style"
+            {...register("name")}
+          />
+          {errors.name && (
+            <span className="span-error">{errors.name.message}!</span>
+          )}
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="input-area">
+          <label className="text-xs font-semibold ">CPF</label>
+          <input
+            type="number"
+            placeholder="898.811.514-73"
+            className="input-style"
+            {...register("cpf")}
+          />
+          {errors.cpf && (
+            <span className="span-error">{errors.cpf.message}!</span>
+          )}
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div className="input-area">
+          <label className="text-xs font-semibold ">Data de Nascimento</label>
+          <input
+            type="number"
+            placeholder="04/08/2006"
+            className="input-style"
+            {...register("birth")}
+          />
+          {errors.birth && (
+            <span className="span-error">{errors.birth.message}!</span>
+          )}
+        </div>
+
+        <div className="input-area">
+          <label className="text-xs font-semibold ">Contato</label>
+          <input
+            type="number"
+            placeholder="(67) 98951-8118"
+            className="input-style"
+            {...register("contact")}
+          />
+          {errors.contact && (
+            <span className="span-error">{errors.contact.message}!</span>
+          )}
+        </div>
+
+        <div className="input-area">
+          <label className="text-xs font-semibold ">Nome da mãe</label>
+          <input
+            type="text"
+            placeholder="Josefa Isabela"
+            className="input-style"
+            {...register("name")}
+          />
+          {errors.name && (
+            <span className="span-error">{errors.name.message}!</span>
+          )}
+        </div>
+
+        <div className="input-area">
+          <label className="text-xs font-semibold ">CEP</label>
+          <input
+            type="text"
+            placeholder="79105-568"
+            className="input-style"
+            {...register("address.zipCode")}
+          />
+          {errors.address && (
+            <span className="span-error">{errors.address.message}!</span>
+          )}
+        </div>
+
+        <div className="input-area">
+          <label className="text-xs font-semibold ">E-mail</label>
+          <input
+            type="text"
+            placeholder="caue_ian_araujo@sobraer.com.br"
+            className="input-style"
+            {...register("email")}
+          />
+
+          {errors.email && (
+            <span className="span-error">{errors.email.message}!</span>
+          )}
+        </div>
+
+        <div className="input-area">
+          <label className="text-xs font-semibold ">Senha</label>
+          <input
+            type="password"
+            placeholder="********"
+            className="input-style"
+            {...register("password")}
+          />
+
+          {errors.password && (
+            <span className="span-error">{errors.password.message}!</span>
+          )}
+        </div>
+
+        <div className="input-area">
+          <label className="text-xs font-semibold ">Confirmar senha</label>
+          <input
+            type="password"
+            placeholder="********"
+            className="input-style"
+            {...register("password")}
+          />
+          {errors.password && (
+            <span className="span-error">{errors.password.message}!</span>
+          )}
+        </div>
+
+        <button
+          type="submit"
+          className="bg-custom-purple-200 rounded-lg font-noto font-normal p-4 text-white mb-24"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          Cadastrar
+        </button>
+      </form>
     </main>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "custom-purple": {
+          200: "#7A5CFA",
+        },
+        "custom-white": {
+          100: "#666",
+          200: "#CCC",
+        },
+        "custom-red": {
+          100: "#EB5757",
+        },
+      },
+      fontFamily: {
+        noto: ["Noto Sans", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
